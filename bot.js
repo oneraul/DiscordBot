10edbd1526db8e1f43a733a4de3217a112ff54b5
oneraul <oneraul@gmail.com>
2018-02-24 23:50:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const insultos_db = require('insultos.json');
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
+
+function getRandomInclusive(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min; }
+
+const GENEROS = [ "masculino", "femenino" ];
+const CATEGORIAS_DE_SUSTANTIVO = [ "mutables", "inmutables" ];
+const CATEGORIAS_DE_ADJETIVO = [ "variables", "invariables" ];
 
 client.on('message', message => {
     if (message.content === 'ping')
@@ -12,15 +19,42 @@ client.on('message', message => {
   	}
 	else if (message.content === 'dado')
 	{
-		var min = 1;
-		var max = 6;
-		var roll = Math.floor(Math.random() * (max - min + 1)) + min;
-		message.reply(roll);
+		message.reply(getRandomInclusive(1, 6));
 	} 
-	else if (message.content === 'nombre aleatorio')
+	else if (message.content === 'insulto')
 	{
-		//message.reply('pene', { tts: true });
-		message.channel.sendMessage(message.author.username + ' lololo', { code: true });
+		//message.channel.sendMessage(message.author.username + ' lololo', { code: true });
+		
+		var genero = GENEROS[getRandomInclusive(0, 1)];
+		var categoria_de_sustantivo = CATEGORIAS_DE_SUSTANTIVO[getRandomInclusive(0, 1)];
+		var categoria_de_adjetivo = CATEGORIAS_DE_ADJECTIVO[getRandomInclusive(0, 1)];
+		
+		var sustantivo;
+		if (categoria_de_sustantivo === "mutables") {
+			var list = insultos_db.sustantivos.mutables[genero];
+			var i = getRandomInclusive(0, list.length-1);
+			sustantivo = list[i] + (genero === "masculino" ? "o" : "a");
+		}
+		else {
+			var list = insultos_db.sustantivos.inmutables;
+			var i = getRandomInclusive(0, list.length-1);
+			sustantivo = list[i];
+		}
+		
+		var adjetivo;
+		if (categoria_de_sustantivo === "variable") {
+			var list = insultos_db.adjetivos.variables;
+			var i = getRandomInclusive(0, list.length-1);
+			adjetivo = list[i] + (genero === "masculino" ? "o" : "a");
+		}
+		else {
+			var list = insultos_db.adjetivos.invariables;
+			var i = getRandomInclusive(0, list.length-1);
+			adjetivo = list[i];
+		}
+		
+		var insulto = sustantivo + " " + adjetivo;
+		message.channel.sendMessage(insulto, { code: true });
 	}
 });
 
